81fa1ef850ccfb91eadb03a519e6ed6a3820c2ed
Cyrus Bhandari <65149993+Cyrus-GHub@users.noreply.github.com>
2023-03-31 18:22:11 +0530

    PISS.sh

---
+++ piss_deb.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+# App switcher only switches through apps in the current workspace
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+
+# Update and upgrade, -y flag added for accepting prompt by default
+sudo apt update && sudo apt upgrade -y
+
+# Install Nala, a front-end for apt
+echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list; wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
+sudo apt update && sudo apt install nala -y
+
+# Use nala instead of apt and apt-get
+echo 'apt() {
+command nala "$@"
+}
+sudo() {
+if [ "$1" = "apt" ] | [ "$1" = "apt-get" ]; then
+shift
+command sudo nala "$@"
+else
+command sudo "$@"
+fi
+apt() {
+command nala "$@"
+}
+sudo() {
+if [ "$1" = "apt" ] | [ "$1" = "apt-get" ]; then
+shift
+command sudo nala "$@"
+else
+command sudo "$@"
+fi' >> ~/.bashrc
+
+echo 'apt() {
+command nala "$@"
+}
+sudo() {
+if [ "$1" = "apt" ] | [ "$1" = "apt-get" ]; then
+shift
+command sudo nala "$@"
+else
+command sudo "$@"
+fi
+apt() {
+command nala "$@"
+}
+sudo() {
+if [ "$1" = "apt" ] | [ "$1" = "apt-get" ]; then
+shift
+command sudo nala "$@"
+else
+command sudo "$@"
+fi' >> /root/.bashrc
+
+# Install packages required for the rest of the script
+sudo apt install software-properties-common apt-transport-https wget -y
+
+# Espanso
+wget https://github.com/federico-terzi/espanso/releases/download/v2.1.8/espanso-debian-x11-amd64.deb
+sudo apt install ./espanso-debian-x11-amd64.deb -y
+espanso service register
+espanso start
+wget https://raw.githubusercontent.com/Cyrus-GHub/piss/main/base.yml
+rm -r /home/cyrus/.config/espanso/match/base.yml
+mv base.yml /home/cyrus/.config/espanso/match/
+
+# Trackpad Gestures
+sudo gpasswd -a $USER input
+sudo apt install wmctrl xdotool -y
+sudo apt install libinput-tools -y
+git clone https://github.com/bulletmark/libinput-gestures.git
+cd libinput-gestures
+sudo make install-y
+cd ~
+
+# Keyboard Config
+wget https://raw.githubusercontent.com/Cyrus-GHub/piss/main/keybindings.dconf
+wget https://raw.githubusercontent.com/Cyrus-GHub/piss/main/custom-keybindings.dconf
+dconf load '/org/gnome/desktop/wm/keybindings/' < keybindings.dconf
+dconf load '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' < custom-keybindings.dconf
+
+# Brave
+sudo apt install curl -y
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser -y
+
+# VSCode
+sudo apt-get install wget gpg -y
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg -y
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg -y
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list' -y
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https -y
+sudo apt update
+sudo apt install code -y
+
+# Github Desktop
+sudo apt install git -y
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.1.1-linux1/GitHubDesktop-linux-3.1.1-linux1.deb
+sudo apt install gdebi -y
+sudo gdebi GitHubDesktop-linux-3.1.1-linux1.deb -y
+
+# Anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+shasum -a 256 Anaconda3-2022.10-Linux-x86_64.sh
+bash Anaconda3-2022.10-Linux-x86_64.sh
+
+# Zoom
+wget https://zoom.us/client/5.13.5.431/zoom_amd64.deb
+sudo apt install ./zoom_amd64.deb -y
+
+# Figma
+wget https://github.com/Figma-Linux/figma-linux/releases/download/v0.10.0/figma-linux_0.10.0_linux_amd64.deb
+sudo apt install ./figma-linux_0.10.0_linux_amd64.deb -y
+
+# Fonts
+wget -O Fonts.zip https://github.com/Cyrus-GHub/piss/blob/main/Fonts%20\(aio\).zip?raw=true
+sudo apt install unzip -y
+unzip Fonts.zip
+sudo cp /home/cyrus/'Fonts (aio)'/*.otf /usr/share/fonts/opentype
+sudo cp /home/cyrus/'Fonts (aio)'/*.ttf /usr/share/fonts/truetype
+
+# Cloudfare Warp
+wget https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2023_1_133_1_amd64_dc941b82de.deb
+sudo apt install ./cloudflare_warp_2023_1_133_1_amd64_dc941b82de.deb -y
+warp-cli register
+
+# CopyPath
+sudo apt install python3-nautilus python3-gi -y
+git clone https://github.com/chr314/nautilus-copy-path.git
+cd nautilus-copy-path
+make install
+nautilus -q
+
+# Install non-free codecs, gnome-tweaks application, VLC media player and Shotwell Image Viewer
+sudo apt install gnome-tweaks vlc shotwell -y
+
+
+<<com
+What's left, apart from a reboot?
+
+1. espanso edit
+2. Add Git SSH key
+3. Gnome Extensions: Clipboard Indicator, Color Picker
+4. CTT config -> Performance, Security,QEMU
+5. Startup Applications:
+rfkill block bluetooth
+system76-power profile performance (Only on PopOS)
+6. Remove the default 4-finger gestures to avoid clashes:
+sudo gedit /usr/share/touchegg/touchegg.conf (Only on PopOS)
+7. Just in case. Windows ISO Flasher for Linux- WoeUSB-ng:
+sudo apt install git p7zip-full python3-pip python3-wxgtk4.0 grub2-common grub-pc-bin
+sudo pip3 install ng-WoeUSB
+com
---
+++ piss_rhel.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# Update and upgrade, -y flag for accepting prompt by default
+sudo dnf update && sudo dnf upgrade -y
+
+# Install packages required for the rest of the script
+sudo dnf install wget -y
+
+# Espanso
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
+sudo dnf install @development-tools gcc-c++ wl-clipboard libxkbcommon-devel dbus-devel wxGTK3-devel -y
+cargo install --force cargo-make --version 0.34.0 -y
+git clone https://github.com/federico-terzi/espanso
+cd espanso
+cargo make --profile release --env NO_X11=true build-binary -y
+sudo mv target/release/espanso /usr/local/bin/espanso
+sudo setcap "cap_dac_override+p" $(which espanso)
+espanso service register
+espanso start
+cd ~
+
+# Keyboard Config
+wget https://raw.githubusercontent.com/Cyrus-GHub/piss/main/keybindings.dconf
+wget https://raw.githubusercontent.com/Cyrus-GHub/piss/main/custom-keybindings.dconf
+dconf load '/org/gnome/desktop/wm/keybindings/' < keybindings.dconf
+dconf load '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' < custom-keybindings.dconf
+
+# Brave
+sudo dnf install dnf-plugins-core -y
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser -y
+
+# VSCode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc -y
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo' -y
+dnf check-update
+sudo dnf install code -y
+
+# Github Desktop
+sudo dnf update
+sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key -y
+sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/yum.repos.d/shiftkey-packages.repo' -y
+sudo dnf update
+sudo dnf install github-desktop -y
+
+# Anaconda
+curl --output anaconda.sh https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh -y
+chmod +x anaconda.sh -y
+bash anaconda.sh
+source ~/.bashrc
+
+# Zoom
+wget https://zoom.us/client/5.14.0.1720/zoom_x86_64.rpm
+sudo dnf localinstall zoom_x86_64.rpm -y
+
+# Figma
+wget https://github.com/Figma-Linux/figma-linux/releases/download/v0.10.0/figma-linux_0.10.0_linux_x86_64.rpm
+sudo dnf localinstall ./figma-linux_0.10.0_linux_x86_64.rpm -y
+
+# Fonts
+wget -O Fonts.zip https://github.com/Cyrus-GHub/piss/blob/main/Fonts%20\(aio\).zip?raw=true
+sudo dnf install unzip -y
+unzip Fonts.zip
+sudo cp /home/cyrus/'Fonts (aio)'/*.otf /usr/share/fonts/opentype
+sudo cp /home/cyrus/'Fonts (aio)'/*.ttf /usr/share/fonts/truetype
+
+# Cloudfare Warp
+wget https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2023_3_258_1_x86_64_7e640027c4.rpm
+sudo dnf localinstall ./cloudflare_warp_2023_3_258_1_x86_64_7e640027c4.rpm -y
+warp-cli register
+
+# CopyPath
+sudo dnf install nautilus-python python3-gobject -y
+git clone https://github.com/chr314/nautilus-copy-path.git
+cd nautilus-copy-path
+make install
+nautilus -q
+
+
+<<com
+What's left, apart from a reboot?
+
+1. espanso edit
+2. Add Git SSH key
+3. Install gnome-tweaks vlc shotwell
+4. Gnome Extensions: Clipboard Indicator, Color Picker
+5. CTT config -> Performance, Security,QEMU
+6. Startup Applications:
+rfkill block bluetooth
+system76-power profile performance (Only on PopOS)
+7. Remove the default 4-finger gestures to avoid clashes:
+sudo gedit /usr/share/touchegg/touchegg.conf (Only on PopOS)
+8. Just in case. Windows ISO Flasher for Linux- WoeUSB-ng:
+sudo apt install git p7zip-full python3-pip python3-wxgtk4.0 grub2-common grub-pc-bin
+sudo pip3 install ng-WoeUSB
+com
